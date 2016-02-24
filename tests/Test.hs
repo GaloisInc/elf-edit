8d91cabd0b0992c184f0685736e735175b3a5e12
Tristan Ravitch <tristan@nochair.net>
2016-02-23 17:11:26 -0800

    Add a test for the stringTable function
    This function assembles a string table bytestring based on a list of
    input strings.  The test is a quickcheck property that tries to hammer
    on that functionality and ensure that the resulting map and bytestring
    are consistent.

--- tests/Test.hs
+++ tests/Test.hs
@@ -1,45 +1,78 @@
 {-# LANGUAGE RankNTypes #-}
 module Main where
 
+import Control.Applicative
 import qualified Data.ByteString as B
+import qualified Data.ByteString.Char8 as C8
 import qualified Data.ByteString.Lazy as LB
-import           Data.Elf
-import           System.IO
-import           Test.HUnit
+import qualified Data.Map as Map
+import Data.Word ( Word32 )
+import qualified System.IO as IO
+import qualified Test.Tasty as T
+import qualified Test.Tasty.HUnit as T
+import qualified Test.Tasty.QuickCheck as T
 
-testEmptyElf :: Assertion
-testEmptyElf = withBinaryFile "./tests/empty.elf" ReadMode $ \h -> do
+import Prelude
+
+import           Data.Elf
+
+testEmptyElf :: T.Assertion
+testEmptyElf = IO.withBinaryFile "./tests/empty.elf" IO.ReadMode $ \h -> do
   fil <- B.hGetContents h
   case parseElf fil of
     Left  _e -> return ()
-    Right _a -> assertFailure "Empty ELF did not cause an exception."
+    Right _a -> T.assertFailure "Empty ELF did not cause an exception."
 
-testIdentityTransform :: FilePath -> Assertion
+testIdentityTransform :: FilePath -> T.Assertion
 testIdentityTransform fp = do
   bs <- B.readFile fp
   withElf bs $ \e -> do
     int0 <- elfInterpreter e
-    assertEqual "Interpreter Name" (Just "/lib64/ld-linux-x86-64.so.2\0") int0
+    T.assertEqual "Interpreter Name" (Just "/lib64/ld-linux-x86-64.so.2\0") int0
     withElf (LB.toStrict (renderElf e)) $ \e' -> do
-      assertEqual "Segment Count" (length (elfSegments e)) (length (elfSegments e'))
+      T.assertEqual "Segment Count" (length (elfSegments e)) (length (elfSegments e'))
       let ex = concat (parseSymbolTables e)
           act = concat (parseSymbolTables e')
-      assertEqual "Symbol table sizes" (length ex) (length act)
+      T.assertEqual "Symbol table sizes" (length ex) (length act)
       int1 <- elfInterpreter e'
-      assertEqual "Interpreter" int0 int1
+      T.assertEqual "Interpreter" int0 int1
 
-withElf :: B.ByteString -> (forall w . Elf w -> Assertion) -> Assertion
+stringTableConsistencyProp :: [AsciiString] -> Bool
+stringTableConsistencyProp strings =
+  all (checkStringTableEntry bytes) (Map.toList tab)
+  where
+    (bytes, tab) = stringTable (map unwrapAsciiString strings)
+
+checkStringTableEntry :: C8.ByteString -> (String, Word32) -> Bool
+checkStringTableEntry bytes (str, off) = str == C8.unpack bstr
+  where
+    bstr = C8.take (length str) $ C8.drop (fromIntegral off) bytes
+
+withElf :: B.ByteString -> (forall w . Elf w -> T.Assertion) -> T.Assertion
 withElf bs f =
   case parseElf bs of
-    Left e -> assertFailure ("Failed to parse elf file: " ++ show e)
+    Left e -> T.assertFailure ("Failed to parse elf file: " ++ show e)
     Right (Elf32 e32) -> f e32
     Right (Elf64 e64) -> f e64
 
-tests :: Test
-tests = TestList
-    [ TestLabel "Empty ELF" $ TestCase testEmptyElf
-    , TestLabel "Identity Transformation (simple)" $ TestCase (testIdentityTransform "./tests/simple.elf")
+tests :: T.TestTree
+tests = T.testGroup "ELF Tests"
+    [ T.testCase "Empty ELF" testEmptyElf
+    , T.testCase "Identity Transformation (simple)" (testIdentityTransform "./tests/simple.elf")
+    , T.testProperty "stringTable consistency" stringTableConsistencyProp
     ]
 
-main :: IO Counts
-main = runTestTT tests
+main :: IO ()
+main = T.defaultMain tests
+
+newtype AsciiString = AsciiString { unwrapAsciiString :: String }
+                    deriving (Show)
+
+instance T.Arbitrary AsciiString where
+  arbitrary = AsciiString <$> genAsciiString
+
+genAsciiString :: T.Gen String
+genAsciiString = T.listOf genAsciiChar
+
+genAsciiChar :: T.Gen Char
+genAsciiChar = T.elements (['a'..'z'] ++ ['A'..'Z'])
